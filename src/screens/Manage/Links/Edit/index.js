ae5ac13d410a453cc108b0a5c0bcb55eebd1d938
Joao Mesquita <joaovictorengp@gmail.com>
2020-07-05 22:23:41 -0300

    Form components

--- src/screens/Manage/Links/Edit/index.js
+++ src/screens/Manage/Links/Edit/index.js
@@ -1,28 +1,31 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import Layout from '../../../Layouts/Manage';
+import FormGroup from '../../../../components/FormGroup';
+import FormCheck from '../../../../components/FormCheck';
 
-export default function Edit() {
+import { linkGet } from '../../../../actions/LinkActions';
+
+const Edit = ({ link, linkGet }) => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    linkGet(id);
+  }, [id, linkGet]);
+
+  console.log('***Edit.id', id);
+  console.log('***Edit.link', link);
+
   return (
     <Layout>
       <h1>Edit Link</h1>
       <div>
         <form>
-          <div className='form-group'>
-            <label>Label</label>
-            <input type='text' className='form-control' />
-          </div>
-          <div className='form-group'>
-            <label>Url</label>
-            <input type='text' className='form-control' />
-          </div>
-          <div className='form-group form-check'>
-            <label className='form-check-label'>
-              <input type='checkbox' name='isSocial' />
-              <span className='form-check-sign'></span>
-              Is Social
-            </label>
-          </div>
+          <FormGroup label='Label' name='label' data={link} type='text' />
+          <FormGroup label='Url' name='url' data={link} type='text' />
+
+          <FormCheck label='Is Social' name='isSocial' data={link} />
           <div>
             <button className='btn btn-primary btn-round'>Submit</button>
           </div>
@@ -30,4 +33,10 @@ export default function Edit() {
       </div>
     </Layout>
   );
-}
+};
+
+const mapStateToProps = (state) => {
+  return { link: state.link.link };
+};
+
+export default connect(mapStateToProps, { linkGet })(Edit);
